182e6c0515d8804e3abe5319b5373db51cda07e2
Apple <apple@renadas-mac.local>
2021-12-04 21:54:05 +0400

    my miniproject 2

---
+++ MiniProject2.py
@@ -0,0 +1,71 @@
+from appledevices import *
+
+start = True
+print("Apple inventory\n------------------") 
+#takes user's information
+#user=input("customer's name: ")
+#making a list of devices
+devices=['iPhone','iPad','MacBook']
+
+
+while start:
+    #printing the list for the user to choose the purchase
+    for i in range(len(devices)):
+        print(f"{i+1} {devices[i]}")
+    print("")
+    #asking for input& storing it in a variable
+    itemIndex = int(input('please select a device you want to purchase:'))
+    #order item name
+    orderItem = devices[itemIndex -1] #gets the order item
+    print(orderItem)
+    #comparing with if statements
+    if orderItem=='iPhone':
+        model = input("iphone X or 12?")
+        if model.lower() != "x" or "12": #if not equal to any set a default choice
+            model = "x"
+        memory= input("64 or 128 GB of memory?")
+        if memory != '64' or '128':
+            memory = '64'
+        sim = input("dual sim? enter True or False: ")
+        if sim.lower() != "true" or "false": #boolean
+            sim = "false"
+        your_iPhone = iPhone(model, memory, sim) #created new iphone
+        print(your_iPhone)
+
+    #ipad
+    #comparing with if statements
+    if orderItem=='iPad':
+        model = input("iPad pro or max?")
+        if model.lower() != "pro" or "12": #if not equal to any set a default choice
+            model = "x"
+        memory=input("64 or 128 GB of memory?")
+        if memory != '64' or '128':
+            memory = '64'
+        pencil = input("would you like to purchase the pencil too? enter True or False: ")
+        if pencil.lower() != "true" or "false": #boolean
+            pencil = "false"
+        your_iPad = iPad(model, memory, pencil) #created new ipad 
+        print(your_iPad)
+
+    #macbook
+    #comparing with if statements
+    if orderItem=='MacBook':
+        model = input("MacBook AIR or PRO?")
+        if model.lower() != "air" or "pro": #set default choice if not equal to
+            model = "air"
+        memory=input("128 or 512 GB of memory?")
+        if memory != "512" or "128":
+            memory = '128'
+        your_MacBook = MacBook(model, memory) #created new macbook
+        print(your_MacBook)
+    
+    proceed = input("would you like to purchase another item? yes or no? ") #prompts the user whether to try again or not
+    if (proceed.lower() == "yes"):
+        start= True #restarts the loop
+    else:
+        start = False #ends
+
+    
+
+
+
---
+++ appledevices.py
@@ -0,0 +1,68 @@
+#base class 
+class apple():
+    def __init__(self,model_name,memory):
+        self.model_name=model_name
+        self.memory=memory
+#inherited classes
+class iPad (apple):
+    def __init__(self, model_name, memory,pencil): #parameters
+        super().__init__(model_name, memory)
+        self.pencil=pencil
+
+    def get_price(self): 
+        self.price = 1500 #minimum price
+        if(self.model_name.lower() == "max"): #inc price depending on choices
+            self.price += 200
+        if(self.memory == '128'):
+            self.price += 200
+        if(self.pencil == True):
+            self.price += 150
+        return self.price
+    def __str__(self) -> str: #returns the description
+        return f"This Item costs ${self.get_price()}, Details - MacBook{self.model_name}','{self.memory}GB','contains a pencil {self.pencil}'\n' 'thank you for your purchase!'"
+
+
+#inherits from base class
+class iPhone (apple):
+    def __init__(self, model_name, memory,dual_sim): #parameters
+        super().__init__(model_name, memory)
+        self.dual_sim=dual_sim
+    
+    def get_price(self):
+        self.price = 800 #minimum price
+        if(self.model_name.lower() == "12"):
+            self.price += 99 #inc price depending on choices
+        if(self.memory == '256'):
+            self.price += 50
+        if(self.dual_sim == True):
+            self.price += 50
+        return self.price
+#returns the description
+    def __str__(self) -> str:
+        return f"This Item costs ${self.get_price()}, Details - iPhone{self.model_name}',' '{self.memory}GB'','' contains dual_sim {self.dual_sim}'\n thank you for your purchase!'"
+
+class MacBook (apple): #inherits from base class
+    def __init__(self, model_name, memory): #parameters
+        super().__init__(model_name, memory)
+
+    def get_price(self):
+        self.price = 2000 #minimum price
+        if(self.model_name.lower() == "pro"):
+            self.price += 200 #inc price depending on choices
+        if(self.memory == '512'):
+            self.price += 100
+        return self.price
+#returns the description
+    def __str__(self) -> str:
+        return f"This Item costs ${self.get_price()}, Details - MacBook{self.model_name}','{self.memory}GB'\n thank you for your purchase!'"
+
+
+
+    
+        
+        
+
+        
+
+   
+
